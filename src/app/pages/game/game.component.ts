60559cabc99fde7e57b9f216aa008f9b3a435ec4
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-07-18 09:18:44 +0200

    hangman game bord

---
+++ src/app/pages/game/game.component.ts
@@ -0,0 +1,106 @@
+import { CategoriesService } from '../../services/categories.service';
+import { Category, Item } from '../../model/index';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css'],
+})
+export class GameComponent implements OnInit {
+  life: number = 8;
+  progressWidth: number = 100;
+  isCompleted: boolean = false;
+  isPopupOpen: boolean = false;
+  alphabet: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  word: string[] = [];
+  displayedWord: string[] = [];
+  category: string = '';
+  activeGame: Category = {
+    name: '',
+    items: [],
+  };
+  question: { name: string; selected: boolean } = { name: '', selected: false };
+  guessedLetters: Set<string> = new Set();
+
+  constructor(
+    private router: ActivatedRoute,
+    private categoriesService: CategoriesService,
+    private route: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.router.paramMap.subscribe((params) => {
+      this.category = params.get('game') || '';
+      if (this.category !== '') {
+        this.category = this.category.split('_').join(' ');
+        this.loadGame();
+      }
+    });
+
+    this.loadGame();
+    this.restartGame();
+  }
+
+  toggleOptions() {
+    console.log('Options toggled');
+  }
+
+  loadGame() {
+    this.activeGame = this.categoriesService.getSelectedCategory(this.category);
+  }
+
+  selectNewCategory() {
+    this.route.navigate(['/categories']);
+  }
+
+  handleLetterClick(letter: string) {
+    if (!this.guessedLetters.has(letter)) {
+      if (this.word.includes(letter)) {
+        for (let i = 0; i < this.word.length; i++) {
+          if (this.word[i] === letter) {
+            this.displayedWord[i] = letter;
+          }
+        }
+
+        if (this.displayedWord.join('') === this.word.join('')) {
+          this.isCompleted = true;
+          this.isPopupOpen = true;
+        }
+      } else {
+        this.life--;
+        this.progressWidth = (this.life / 8) * 100;
+        if (this.life === 0) {
+          this.isPopupOpen = true;
+        }
+      }
+
+      this.guessedLetters.add(letter);
+    }
+  }
+
+  resumeGame() {
+    this.isPopupOpen = false;
+  }
+
+  exitGame() {
+    this.isPopupOpen = false;
+    this.route.navigate(['/']);
+  }
+
+  restartGame() {
+    this.life = 8;
+    this.isPopupOpen = false;
+    this.guessedLetters.clear();
+
+    const randomItem =
+      this.activeGame.items[
+        Math.floor(Math.random() * this.activeGame.items.length)
+      ];
+
+    this.word = randomItem.name.toUpperCase().split('');
+    this.displayedWord = Array(this.word.length).fill('');
+    this.progressWidth = 100;
+  }
+}
