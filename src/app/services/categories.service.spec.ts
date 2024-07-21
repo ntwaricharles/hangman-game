6a3cd9b042e3b754e77a5e6519cb31f4dc9d722e
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-07-21 22:44:26 +0200

    added tests for categories services

--- src/app/services/categories.service.spec.ts
+++ src/app/services/categories.service.spec.ts
@@ -1,6 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CategoriesService } from './categories.service';
+import DATA from '../../data.json';
+
+// Define the Categories type
+type Categories = {
+  [key: string]: { name: string; selected: boolean }[];
+};
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
@@ -12,5 +17,32 @@ describe('CategoriesService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return categories data', () => {
+    const categories = service.getCategories();
+    expect(categories).toEqual(DATA.categories);
+  });
+
+  it('should return selected category', () => {
+    const categoryName = 'Movies'; // Use an existing category name
+    const expectedCategory = {
+      name: categoryName,
+      items: (DATA.categories as Categories)[categoryName],
+    };
+
+    const selectedCategory = service.getSelectedCategory(categoryName);
+    expect(selectedCategory).toEqual(expectedCategory);
+  });
+
+  it('should return empty category if not found', () => {
+    const categoryName = 'nonExistentCategory';
+    const expectedCategory = {
+      name: '',
+      items: [],
+    };
+
+    const selectedCategory = service.getSelectedCategory(categoryName);
+    expect(selectedCategory).toEqual(expectedCategory);
   });
 });
