3bdf5467fbf863511d6740b9a0cada51bba56771
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-07-21 22:36:50 +0200

    added tests for home page

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,17 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let mockRouter: jest.Mocked<Router>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HomeComponent]
-    })
-    .compileComponents();
+    // Create a mock object for Router
+    mockRouter = {
+      navigate: jest.fn(),
+    } as unknown as jest.Mocked<Router>;
 
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+      providers: [{ provide: Router, useValue: mockRouter }],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +27,15 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to categories when goToCategories is called', () => {
+    component.goToCategories();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/categories']);
+  });
+
+  it('should navigate to guidance when goToGuidence is called', () => {
+    component.goToGuidence();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/guidance']);
   });
 });
